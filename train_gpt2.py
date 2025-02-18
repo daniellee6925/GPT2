c2c9938422b5dc401436958e7105f8a5ec2eba94
guya lee <daniellee6925@gmail.com>
2025-02-18 08:51:36 -0800

    fix errors in transformers module dict

--- train_gpt2.py
+++ train_gpt2.py
@@ -7,15 +7,6 @@ import math
 # -----------------------------------------
 
 
-@dataclass
-class GPTConfig:
-    block_size: int = 1024
-    vocab_size: int = 50257
-    n_layer: int = 12
-    n_head: int = 12
-    n_embd: int = 768
-
-
 class CasualSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -83,6 +74,15 @@ class Block(nn.Module):
         return x
 
 
+@dataclass
+class GPTConfig:
+    block_size: int = 1024  # max sequence length
+    vocab_size: int = 50257  # number of tokens: 50,000 BPE merges + 256 bytes tokens + 1 <|endoftext|> token
+    n_layer: int = 12  # number of layers
+    n_head: int = 12  # number of heads
+    n_embd: int = 768  # embedding dimension
+
+
 class GPT(nn.Module):
     def __init__(self, config):
         super().__init__()
@@ -91,7 +91,7 @@ class GPT(nn.Module):
         self.transformer = nn.ModuleDict(
             dict(
                 wte=nn.Embedding(config.vocab_size, config.n_embd),
-                wpe=nn.Embedding(config.vocab_size, config.n_embd),
+                wpe=nn.Embedding(config.block_size, config.n_embd),
                 h=nn.ModuleList([Block(config) for _ in range(config.n_layer)]),
                 ln_f=nn.LayerNorm(config.n_embd),
             )
@@ -99,65 +99,70 @@ class GPT(nn.Module):
         # last linear layer to match convert Channel dim to vocab_size
         self.lm_head = nn.Linear(config.n_embd, config.vocab_size, bias=False)
 
-        @classmethod
-        def from_pretrained(cls, model_type):
-            """loads pretrained GPT-2 model weights from hugging face"""
-            assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
-            from transformers import GPT2LMHeadModel
+    @classmethod
+    def from_pretrained(cls, model_type):
+        """loads pretrained GPT-2 model weights from hugging face"""
+        assert model_type in {"gpt2", "gpt2-medium", "gpt2-large", "gpt2-xl"}
+        from transformers import GPT2LMHeadModel
 
-            print("loading wieghts from pretrained gpt: %s" % model_type)
+        print("loading wieghts from pretrained gpt: %s" % model_type)
 
-            # n_layer, n_embd, n_head are determined from model type
-            config_args = {
-                "gpt2": dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
-                "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),  # 350M params
-                "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),  # 774M params
-                "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M params
-            }[model_type]
+        # n_layer, n_embd, n_head are determined from model type
+        config_args = {
+            "gpt2": dict(n_layer=12, n_head=12, n_embd=768),  # 124M params
+            "gpt2-medium": dict(n_layer=24, n_head=16, n_embd=1024),  # 350M params
+            "gpt2-large": dict(n_layer=36, n_head=20, n_embd=1280),  # 774M params
+            "gpt2-xl": dict(n_layer=48, n_head=25, n_embd=1600),  # 1558M params
+        }[model_type]
 
-            config_args["vocab_size"] = 50257
-            config_args["block_size"] = 1024
+        config_args["vocab_size"] = 50257
+        config_args["block_size"] = 1024
 
-            # create from scratch initialized minGPT model
-            config = GPTConfig(**config_args)
-            model = GPT(config)
-            sd = model.state_dict()
-            sd_keys = sd.keys()
+        # create from scratch initialized minGPT model
+        config = GPTConfig(**config_args)
+        model = GPT(config)
+        sd = model.state_dict()
+        sd_keys = sd.keys()
 
-            # ignore the buffers
-            sd_keys = [k for k in sd_keys if not k.endswith(".attn.bias")]
+        # ignore the buffers
+        sd_keys = [k for k in sd_keys if not k.endswith(".attn.bias")]
 
-            # initialized hugging face transformers model
-            model_hf = GPT2LMHeadModel.from_pretrained(model_type)
-            sd_hf = model_hf.state_dict()
+        # initialized hugging face transformers model
+        model_hf = GPT2LMHeadModel.from_pretrained(model_type)
+        sd_hf = model_hf.state_dict()
 
-            # copy while ensuring all parameters are aligned and match in names and shapes
-            sd_keys_hf = sd_hf.keys()
-            # ignore buffers and masks
-            sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.masked_bias")]
-            sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.bias")]
+        # copy while ensuring all parameters are aligned and match in names and shapes
+        sd_keys_hf = sd_hf.keys()
+        # ignore buffers and masks
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.masked_bias")]
+        sd_keys_hf = [k for k in sd_keys_hf if not k.endswith(".attn.bias")]
 
-            # manually transpose the weights if it's from tensorflow
-            transposed = [
-                "attn.c_weight",
-                "attn.c_proj.wieght",
-                "mlp.c_fc.weight, mlp.c_proj.weight",
-            ]
+        # manually transpose the weights if it's from tensorflow
+        transposed = [
+            "attn.c_attn.weight",
+            "attn.c_proj.weight",
+            "mlp.c_fc.weight",
+            "mlp.c_proj.weight",
+        ]
 
-            assert len(sd_keys_hf) == len(sd_keys), (
-                f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
-            )
-            for k in sd_keys_hf:
-                if any(k.endswith(w) for w in transposed):
-                    # special treatment for the Conv1D weights we need to transpose
-                    assert sd_hf[k].shape[::-1] == sd[k].shape
-                    with torch.no_grad():
-                        sd[k].copy_(sd_hf[k].t())
+        assert len(sd_keys_hf) == len(sd_keys), (
+            f"mismatched keys: {len(sd_keys_hf)} != {len(sd_keys)}"
+        )
+        for k in sd_keys_hf:
+            if any(k.endswith(w) for w in transposed):
+                # special treatment for the Conv1D weights we need to transpose
+                assert sd_hf[k].shape[::-1] == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k].t())
 
-                else:
-                    # vanilla copy over the other parameters
-                    assert sd_hf[k].shape == sd[k].shape
-                    with torch.no_grad():
-                        sd[k].copy_(sd_hf[k])
+            else:
+                # vanilla copy over the other parameters
+                assert sd_hf[k].shape == sd[k].shape
+                with torch.no_grad():
+                    sd[k].copy_(sd_hf[k])
 
-            return model
+        return model
+
+
+model = GPT.from_pretrained("gpt2")
+print("didn't crash")
