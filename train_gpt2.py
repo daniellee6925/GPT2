cc010343cd2a2054c44097f48ae96abb85ef7969
guya lee <daniellee6925@gmail.com>
2025-02-19 23:45:54 -0800

    create check if loaded weights match

--- train_gpt2.py
+++ train_gpt2.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 from torch.nn import functional as F
 import math
+import tiktoken
 
 # -----------------------------------------
 
@@ -11,6 +12,7 @@ class CasualSelfAttention(nn.Module):
     def __init__(self, config):
         super().__init__()
         assert config.n_embd % config.n_head == 0
+        # key, query, value projections for all heads, but in a batch
         self.c_attn = nn.Linear(config.n_embd, 3 * config.n_embd)
         # output projection
         self.c_proj = nn.Linear(config.n_embd, config.n_embd)
@@ -21,12 +23,14 @@ class CasualSelfAttention(nn.Module):
                 1, 1, config.block_size, config.block_size
             ),
         )
+        self.n_head = config.n_head
+        self.n_embd = config.n_embd
 
     def forward(self, x):
         B, T, C = x.size()
 
         qkv = self.c_attn(x)
-        q, k, v = qkv.split(self.n_embd, dim=2)
+        q, k, v = qkv.split(self.n_embd, dim=-1)
 
         # (B, nh, T, hs)
         k = k.view(B, T, self.n_head, C // self.n_head).transpose(1, 2)
@@ -108,9 +112,9 @@ class GPT(nn.Module):
 
         # forward the token and positional embeddings
         pos = torch.arange(0, T, dtype=torch.long, device=idx.device)  # shape T
-        pos_emb = self.transformer.wpe(pos)
-        tok_emb = self.transformer.wte(idx)
-        x = pos_emb + tok_emb
+        pos_emb = self.transformer.wpe(pos)  # (T, embd)
+        tok_emb = self.transformer.wte(idx)  # (B, T, embd)
+        x = pos_emb + tok_emb  # (B, T, embd)
         # forward the blocks of the transformer
         for block in self.transformer.h:
             x = block(x)
@@ -184,5 +188,70 @@ class GPT(nn.Module):
         return model
 
 
+# -----------------------------------------------------------------------
+num_return_sequences = 5
+max_length = 30
+from transformers import GPT2LMHeadModel
+
+hf_gpt2 = GPT2LMHeadModel.from_pretrained("gpt2")
+hf_gpt2.to("cuda")
 model = GPT.from_pretrained("gpt2")
-print("didn't crash")
+model.eval()
+model.to("cuda")
+
+sd_hf = model.state_dict()
+print(sd_hf["transformer.h.0.attn.c_attn.weight"].shape)
+
+transposed = [
+    "attn.c_attn.weight",
+    "attn.c_proj.weight",
+    "mlp.c_fc.weight",
+    "mlp.c_proj.weight",
+]
+# Assume `model` is your model and `gpt2_model` is the pretrained GPT-2
+for (name1, param1), (name2, param2) in zip(
+    model.named_parameters(), hf_gpt2.named_parameters()
+):
+    if any(name1.endswith(w) for w in transposed):
+        param1_to_compare = param1.t()  # Transpose the parameter if it matches
+    else:
+        param1_to_compare = param1  # Use original parameter
+    if not torch.allclose(param1_to_compare, param2, atol=1e-6):
+        print(f"🚨 Mismatch in {name1}")
+    else:
+        print(f"✅ {name1} matches!")
+
+"""
+enc = tiktoken.get_encoding("gpt2")
+tokens = enc.encode("Hello, I'm a language model,")
+tokens = torch.tensor(tokens, dtype=torch.long)  # (8, )
+tokens = tokens.unsqueeze(0).repeat(num_return_sequences, 1)  # (5, 8)
+x = tokens.to("cuda")
+
+
+# generate
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+while x.size(1) < max_length:
+    with torch.no_grad():
+        logits = model(x)  # (B, T, vocab_size)
+        # take the logits at the last location
+        logits = logits[:, -1, :]  # (B, vocab_size)
+        probs = F.softmax(logits, dim=-1)
+        # do top-k sampling of 50
+        topk_probs, topk_indices = torch.topk(probs, 50, dim=-1)  # (5, 50)
+        # select a token from the top k probs
+        ix = torch.multinomial(topk_probs, 1)  # (B, 1)
+        # gather the corresponding indices
+        xcol = torch.gather(topk_indices, -1, ix)  # (B, 1)
+        # append to sequence
+        x = torch.cat((x, xcol), dim=1)  # (B, i+1)
+        # will have (5, 30) at the end of while loop
+
+# print the results
+for i in range(num_return_sequences):
+    tokens = x[i, :max_length].tolist()
+    decoded = enc.decode(tokens)
+    print(">", decoded)
+"""
